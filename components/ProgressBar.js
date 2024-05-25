7a5c27de9aa5c982b1bc24fa0817bf5461942dbe
Eileen Sam <eileendsam@gmail.com>
2024-05-25 17:48:45 -0400

    Progress bar design

--- components/ProgressBar.js
+++ components/ProgressBar.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import * as Progress from 'react-native-progress';
 import allWords from '../constants/c_words';
 
 const ProgressBar = ({ guessedWords }) => {
   const progress = guessedWords.size / allWords.size;
+  const totalBlocks = 20; // Number of blocks in the progress bar
+  const filledBlocks = Math.round(progress * totalBlocks);
+  const emptyBlocks = totalBlocks - filledBlocks;
 
   return (
     <View style={styles.container}>
-      <Progress.Bar progress={progress} width={200} />
+      <View style={styles.progressBar}>
+        {[...Array(filledBlocks)].map((_, index) => (
+          <View key={index} style={[styles.block, styles.filledBlock]} />
+        ))}
+        {[...Array(emptyBlocks)].map((_, index) => (
+          <View key={index} style={[styles.block, styles.emptyBlock]} />
+        ))}
+      </View>
       <Text style={styles.progressText}>{`${guessedWords.size} / ${allWords.size}`}</Text>
     </View>
   );
@@ -18,6 +27,21 @@ const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
     marginBottom: 20,
+  },
+  progressBar: {
+    flexDirection: 'row',
+    marginBottom: 10,
+  },
+  block: {
+    width: 10, // Width of each block
+    height: 10, // Height of each block
+    marginRight: 2,
+  },
+  filledBlock: {
+    backgroundColor: '#0065bd',
+  },
+  emptyBlock: {
+    backgroundColor: '#ccc',
   },
   progressText: {
     fontSize: 18,
